c07d7287b386d94c4c2c3bdb843a06fc414be045
TV GAME.DEV <tvgame.dev@hotmail.com>
2025-05-01 13:57:19 +0300

    Create env.js

---
+++ env.js
@@ -0,0 +1,6 @@
+window.CLERK_ENV = {
+    PUBLISHABLE_KEY: "",
+    SCRIPT_SRC: "https://clerk-url/npm/@clerk/clerk-js@5/dist/clerk.browser.js"
+  };
+  
+// To protect the code, you can use: https://obfuscator.io/
